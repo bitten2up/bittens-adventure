c48472e51133915ce2e1eda2ae04d78ca18d6eea
Litten 1up <seant9140@gmail.com>
2021-12-06 16:27:51 -0600

    Add files via upload

---
+++ src/bittendef.h
@@ -0,0 +1,8 @@
+/**
+* this file toggles debugging and release flags for the game
+*/
+#ifndef bittendef
+#define bittendef
+// enables debugging features, disable this for releases
+#define debug
+#endif
