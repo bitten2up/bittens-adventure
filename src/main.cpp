86468731702549612c477f21b236a07fc5f02bb2
Litten 1up <seant9140@gmail.com>
2021-12-15 17:33:49 -0600

    forgot to include version.h

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #include <string>
 // #include <fstream.h>
 #include "bittendef.h"
-
+#include "version.h"
 ////////////////////////////////////////////////////////////
 /// Entry point of application
 ///
