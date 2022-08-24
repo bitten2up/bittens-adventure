4fd44a210d6f22c57c4b77877a26b4722ba30770
bitten 2up <seant9140@gmail.com>
2022-08-24 15:52:43 -0500

    add crappy movement

--- src/bittendef.h
+++ src/bittendef.h
@@ -1,14 +1,12 @@
 /**
 * this file toggles debugging and release flags for the game
 */
-
-#include <string>
-#include <iostream>
-using namespace std;
 #ifndef bittendef
 #define bittendef
 // enables debugging features, disable this for releases
 #define debug
 // enables battle menu test disable this so then you can do other things than the test battle
 //#define battleTest //outdated
+// this is a really nesh flag, it is used in my notepad++ debug compiles
+#define notepadplusplusDebug
 #endif
--- src/settings.h
+++ src/settings.h
@@ -1,38 +1,9 @@
-#include <iostream>
-#include <string>
-#include bittendef
-// used to toggle indivisal debug settings
-class debugsettings{
-    public:
-        bool debug(){
-            #ifdef debug
-            debugmode=true; // used for debug logs and leveledit when I add tilemaps
-            #else 
-            debugmode=false;
-            #endif
-            return debugmode;
-        }
-        bool instentwin=false; //win battles instently
-        bool noclip=false; // tbh everything is noclip when adding this it is for the future
-        bool battletest=false; // replases #define battleTest
-        const float version = 0.01
-        string getCodename(){
-            if (debugmode){ // used for eastereggs when i add them
-                codename ="iced bitten" ;  //define the debug codename;
-            }
-            else{
-                codename = "flaming bitten"
-            }
-            return codename;
-        }
-    private:
-        bool debugmode;
-        string codename;
-};
+#ifndef settings_h
+#define settings_h
+// trust me this file is way to broken to comperhend, i will rewrite this soon
+#include "bittendef.h"
+// used to toggle settings used in debugging
+class debugsettings{};
 // used to toggle settings not usaly used for debugging
-class normalsettings{
-    public: 
-        bool fullscreen = true;
-        string savedata = "null lol";
-        bool bittendev = true;
-        int whatthehell = 1;
+class normalsettings{};
+#endif
