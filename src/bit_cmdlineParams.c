194fbb90b42506e34e0c18e87c23bc5325725aa5
bitten 2up <seant9140@gmail.com>
2022-10-06 19:53:52 -0500

    ah a little update -_^

--- src/bit_cmdlineParams.c
+++ src/bit_cmdlineParams.c
@@ -13,10 +13,13 @@
 // this probly should be part of int main(), but i dont feel like doing it rn even tho it would be easy
 bool cmdlineParams(int argc, char** argv){
     if (argc!=1){
-        printf("\nargc size\n%i\n", argc);
         for (int i = 1; i < argc; ++i) {
             if (strcmp(argv[i], "--version")==0){
                 printf("\nBitten Engine\n\nversion 1.0 indev\n\nfeatures subject to change\n\n\nCopyright (c) 2022 Bitten2up, licened under the M.I.T license check LICENSE\n");
+                return true;
+            }
+            if (strcmp(argv[i], "--iced")==0){
+                printf("cold bitten 0_0\n");
                 return true;
             }
             if (strcmp(argv[i], "battletest")) // return of the battle test
