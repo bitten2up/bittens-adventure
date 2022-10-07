e882a4fbedd7a6508516756ba009420adc31d82b
Bitten 2up <seant9140@gmail.com>
2022-10-06 20:21:39 -0500

    changed some of bittendef

--- src/bittendef.h
+++ src/bittendef.h
@@ -8,12 +8,8 @@
 //#define debugsprites // cuts fps to 15fps, sense i animate at 15fps then add extra frames
 // files
 #define SETTINGS_FILE "assets/settings.cfg"
-// enables battle menu test disable this so then you can do other things than the test battle
-//#define battleTest //outdated
-// this is a really nesh flag, it is used in my notepad++ debug compiles
-//#define notepadplusplusDebug
 
-    // TODO make this editable in an settings menu
+// TODO make this editable in an settings menu
 #define SCREENWIDTH 800
 #define SCREENHEIGHT 450
 ////////////////////////////////////////////////////////////
