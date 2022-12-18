9049b997edc573f2f626d95de1fee4f64926ceb4
bitten 1up <seant9140@gmail.com>
2022-12-18 17:41:06 -0600

    fix stuff...

--- src/dll_testing.c
+++ src/dll_testing.c
@@ -9,7 +9,7 @@
 #include <pthread.h>
 #include "bittendef.h"
 
-int __declspec(dllexport) __cdecl bittenEnginePatch(bit_gamestate* game) {
+int __declspec(dllexport) __cdecl bittenEnginePatch(bit_game* game) {
     game->settings->width=900;
     game->settings->silent=true; // dont show modded text
     printf("Bitten engine patching worked\nscreen width is now: %i", game->settings->width);
