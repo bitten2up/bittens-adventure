542ec65042053329db15b5070f5991d05488e6f8
bitten2up <seant9140@gmail.com>
2023-03-25 16:58:10 -0500

    fix default save

--- src/defaultSave.h
+++ src/defaultSave.h
@@ -2,7 +2,7 @@ unsigned char saveD[] = {
   'b', 'i', 't', 't', 'e', 'n', 's', 'a', 'v', 0xc4, // header
   0x01, // version
   0b10000000, // first value: music, second value: fullscreen
-  0xa1, 0x01, // location (x,y)
+  0x01, 0x01, // location (x,y)
   0x0a
 };
 unsigned int saveDlen = 15;
